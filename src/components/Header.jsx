3522a597379b534a496b04a9f485935ff26a6715
PauloSuriani <paulosuriani@gmail.com>
2022-08-27 09:08:42 -0300

    feat: Header.jsx customiz

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,47 @@
 import React from 'react';
-// import avatar from '../IMG_20210428_185440_243.png'
 class Header extends React.Component {
   render() {
     return (
       <header className='flex container max-w-screen-md'>
-        {/* <img className="w-20 rounded-full" src={avatar} alt="Bordered avatar"/> */}
         <div>
-          <h1 className='pt-3 lg:text-5xl md:text-4xl text-3xl'>Keep in touch</h1>
+          <h1 className='pt-8 lg:text-5xl md:text-4xl text-3xl'>
+            Keep in touch
+          </h1>
           <div className='py-3'>
-            <a className='hover:text-lime-600 pr-2 text-2xl font-serif text-lime-300' href={'https://github.com/PauloSuriani'}>GitHub</a>
+            <a 
+              className='hover:text-lime-600 pr-2 text-2xl font-serif text-lime-300' 
+              href={'paulosuriani88@hotmail.com'}
+            >
+              Email
+            </a>
             <span className='pr-2 font-serif'>|</span>
-            <a className='pr-2 text-2xl font-serif text-lime-300 hover:text-lime-600' href={'https://www.linkedin.com/in/paulo-vitor-moura-suriani-529683221/'}>LinkedIn</a>
+            <a 
+              className='hover:text-lime-600 pr-2 text-2xl font-serif text-lime-300' 
+              href={'https://github.com/PauloSuriani'}
+            >
+              GitHub
+            </a>
             <span className='pr-2 font-serif'>|</span>
-            <a className='pr-2 text-2xl font-serif text-lime-300 hover:text-lime-600' href={'https://www.instagram.com/paulosuriani/'}>Instagram</a>
+            <a 
+              className='pr-2 text-2xl font-serif text-lime-300 hover:text-lime-600' 
+              href={'https://www.linkedin.com/in/paulo-vitor-moura-suriani-529683221/'}
+            >
+              In
+            </a>
             <span className='pr-2 font-serif'>|</span>
-            <a className='text-2xl font-serif text-lime-300 hover:text-lime-600' href={'https://github.com/PauloSuriani'}>Currículo</a>
+            <a 
+              className='pr-2 text-2xl font-serif text-lime-300 hover:text-lime-600' 
+              href={'https://www.instagram.com/paulosuriani/'}
+              >
+                Social
+              </a>
+            <span className='pr-2 font-serif'>|</span>
+            <a 
+              className='text-2xl font-serif text-lime-300 hover:text-lime-600' 
+              href={'https://github.com/PauloSuriani'}
+            >
+              Certifications
+            </a>
           </div>
         </div>
     </header>
